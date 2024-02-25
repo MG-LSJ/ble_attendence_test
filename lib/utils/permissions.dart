139b8d46d085c26882b4cbbd9435f62a5a6311fa
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-02-25 22:01:21 +0530

    podfile updates

--- lib/utils/permissions.dart
+++ lib/utils/permissions.dart
@@ -27,62 +27,57 @@ Future<bool> checkBluetoothPermissions() async {
 
 Future<bool> isBluetoothOn(BuildContext context) async {
   bool RETURN_VALUE = false;
-  if (Platform.isIOS) {
-    return true;
-  }
-  if (Platform.isAndroid) {
-    var status = await Permission.bluetooth.serviceStatus.isEnabled;
 
-    RETURN_VALUE = status;
+  var status = await Permission.bluetooth.serviceStatus.isEnabled;
 
-    print('bluetooth status: $status');
-    if (RETURN_VALUE) {
-      return RETURN_VALUE;
-    } else {
-      if (!context.mounted) return false;
-      await showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) => AlertDialog(
-          title: const Text('Bluetooth is off'),
-          content:
-              const Text('App needs bluetooth to work! Please turn it on.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                // dismiss the dialog
+  RETURN_VALUE = status;
+
+  print('bluetooth status: $status');
+  if (RETURN_VALUE) {
+    return RETURN_VALUE;
+  } else {
+    if (!context.mounted) return false;
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog.adaptive(
+        title: const Text('Bluetooth is off'),
+        content: const Text('App needs bluetooth to work! Please turn it on.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              // dismiss the dialog
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () async {
+              await AppSettings.openAppSettings(
+                type: AppSettingsType.bluetooth,
+              );
+            },
+            child: const Text('Open Settings'),
+          ),
+          TextButton(
+            onPressed: () async {
+              if (await Permission.bluetooth.serviceStatus.isEnabled) {
                 Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () async {
-                await AppSettings.openAppSettings(
-                  type: AppSettingsType.bluetooth,
+                RETURN_VALUE = true;
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                        'Please turn on bluetooth to mark your attendance'),
+                  ),
                 );
-              },
-              child: const Text('Open Settings'),
-            ),
-            TextButton(
-              onPressed: () async {
-                if (await Permission.bluetooth.serviceStatus.isEnabled) {
-                  Navigator.of(context).pop();
-                  RETURN_VALUE = true;
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text(
-                          'Please turn on bluetooth to mark your attendance'),
-                    ),
-                  );
-                }
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
-    }
+              }
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
   }
   return RETURN_VALUE;
 }
